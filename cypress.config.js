90fabc081fc43ee0e20997bf3d001cee42d1286f
Ramiro Brandão <ramirobrandao@outlook.com>
2025-09-17 11:50:38 -0300

    :package: build: Adicionando Mochawesome no package.json e config

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,21 @@ module.exports = defineConfig({
     baseUrl: "https://magento.nublue.co.uk/",
     chromeWebSecurity: false,
     defaultCommandTimeout: 10000,
-
+    reporter: 'mochawesome',
+    screenshotOnRunFailure: true,  //gera print quando um teste falhar
+    "reporterOptions": {
+      "reportDir": "cypress/reports/mochawesome-report",
+      "overwrite": false,
+      "html": true,
+      "json": true,
+      "charts": true,
+      "reportFilename": "report",
+      "timestamp": "mmddyyyy_HHMMss",
+      "inlineAssets": true,
+      "toOpen": true,
+      "reportPageTitle": "Relatório de execução de testes no Luma Ecommerce - Magento",
+      "embeddedScreenshots": true  //integrar prints no relatório
+    },
 
 
     setupNodeEvents(on, config) {
